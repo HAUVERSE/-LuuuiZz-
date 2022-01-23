8ac6aba9780df499e6069173dc8d917a5304e6c3
HAUVERSE <111231310+HAUVERSE@users.noreply.github.com>
2022-01-23 10:15:08 +0000

    chore(package): update eslint to version 4.2.0

--- ml-project/src/main.rs
+++ ml-project/src/main.rs
@@ -4,3 +4,7 @@ use std::fs::File;
 use ndarray::{ Array, Array1, Array2, OwnedRepr, ArrayBase, Dim };
 use linfa::Dataset;
 use plotlib::page::Page;
+use plotlib::repr::Plot;
+use plotlib::style::{PointMarker, PointStyle};
+use plotlib::grid::Grid;
+use plotlib::view::ContinuousView;
