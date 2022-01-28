31c210d2ea361ee41036ef9a9cf918000c320de9
HAUVERSE <111231310+HAUVERSE@users.noreply.github.com>
2022-01-28 13:57:51 +0000

    Update subproject

--- ml-project/src/main.rs
+++ ml-project/src/main.rs
@@ -11,3 +11,8 @@ use plotlib::view::ContinuousView;
 use plotlib::view::View;
 use linfa::prelude::*;
 use linfa_logistic::LogisticRegression;
+
+type MyDataset = DatasetBase<ArrayBase<OwnedRepr<f64>, Dim<[usize; 2]>>, ArrayBase<OwnedRepr<&'static str>, Dim<[usize; 2]>>,>;
+
+fn get_dataset() -> Dataset<f32, i32, ndarray::Dim<[usize;1]>> {
+    let mut reader = Reader::from_path("./src/heart.csv").unwrap();
