3cff6caf90cb76f8062314c9033b7b53b4024374
HAUVERSE <111231310+HAUVERSE@users.noreply.github.com>
2022-02-22 19:27:15 +0000

    ACPI / video: Load the module even if ACPI is disabled

--- ml-project/src/main.rs
+++ ml-project/src/main.rs
@@ -51,3 +51,4 @@ fn get_records(data: &Vec<Vec<f32>>, target_index: usize) -> Array2<f32> {
     let mut records: Vec<f32> = vec![];
     for record in data.iter() {
         records.extend_from_slice( &record[0..target_index] );
+    }
